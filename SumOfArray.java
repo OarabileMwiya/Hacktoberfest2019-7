28e40298f260ddb098375dcec1053dff3415401c
OarabileMwiya <42150074+OarabileMwiya@users.noreply.github.com>
2019-10-24 22:16:36 +0200

    Update SumOfArray.java

--- SumOfArray.java
+++ SumOfArray.java
@@ -1,11 +1,17 @@
 class SumOfArray{
+   
+   //Main meethod
    public static void main(String args[]){
+      //array of integers
       int[] array = {10, 20, 30, 40, 50, 10};
+      
+      //Declaration and initialisation of variable sum
       int sum = 0;
       //Advanced for loop
       for( int num : array) {
           sum = sum+num;
       }
+      // print out or display sum of array
       System.out.println("Sum of array elements is:"+sum);
    }
 }
