80db50e1f2d370179a200d71239e9755b8d9f074
OarabileMwiya <42150074+OarabileMwiya@users.noreply.github.com>
2019-10-24 22:24:46 +0200

    Update Floyd's_triangle.java

--- Floyd's_triangle.java
+++ Floyd's_triangle.java
@@ -1,8 +1,11 @@
 import java.util.Scanner;
 class FloydTriangleExample
-{
+{  
+    //Main Method
     public static void main(String args[])
     {
+        
+        //Declaration and Initialisation of field variables
        int rows, number = 1, counter, j;
        //To get the user's input
        Scanner input = new Scanner(System.in);
@@ -11,6 +14,8 @@ class FloydTriangleExample
        rows = input.nextInt();
        System.out.println("Floyd's triangle");
        System.out.println("****************");
+        
+        //For loop to traverse through the input rows
        for ( counter = 1 ; counter <= rows ; counter++ )
        {
            for ( j = 1 ; j <= counter ; j++ )
